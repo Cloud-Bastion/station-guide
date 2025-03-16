a68cb38fa089418738a53e4afcdc00a4c023356c
Melvin Schneider <melvinschneider02@gmail.com>
2025-03-16 05:16:09 +0100

    Add routings to all views

--- station-frontend/src/router/index.ts
+++ station-frontend/src/router/index.ts
@@ -1,11 +1,23 @@
 import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
-import EmployeeManageView from "@/view/EmployeeManageView.vue";
+import EmployeeManageView from "@/view/employee/EmployeeManageView.vue";
+import MHDManageView from "@/view/MHDManageView.vue";
+import TaskManagementView from "@/view/TaskManagementView.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
-        path: '/employeemanagement',
-        name: 'employeemanagment',
+        path: '/employee-management',
+        name: 'employee-managment',
         component: EmployeeManageView
+    },
+    {
+        path: '/mhd-management',
+        name: 'mhd-management',
+        component: MHDManageView
+    },
+    {
+        path: '/task-management',
+        name: 'task-management',
+        component: TaskManagementView
     }
 ]
 
