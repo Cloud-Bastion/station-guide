99ad4421b68c77b55e9ee2fb570c9c3a8bb65bb3
Melvin Schneider <melvinschneider02@gmail.com>
2025-03-16 05:17:26 +0100

    Add TaskmananageView for task management

---
+++ station-frontend/src/view/TaskManagementView.vue
@@ -0,0 +1,12 @@
+<template>
+  <SidebarComponent site="task-management"/>
+</template>
+
+<script setup lang="ts">
+
+import SidebarComponent from "@/components/sidebar/SidebarComponent.vue";
+</script>
+
+<style lang="scss" module>
+
+</style>
