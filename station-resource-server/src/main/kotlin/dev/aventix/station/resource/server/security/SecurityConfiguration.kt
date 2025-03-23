b43d83051a9425b88177d0004ced0899f054b035
Melvin Schneider <melvinschneider02@gmail.com>
2025-03-23 09:49:53 +0100

    format

--- station-resource-server/src/main/kotlin/dev/aventix/station/resource/server/security/SecurityConfiguration.kt
+++ station-resource-server/src/main/kotlin/dev/aventix/station/resource/server/security/SecurityConfiguration.kt
@@ -6,12 +6,7 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
-
 
 @Configuration
 class SecurityConfiguration {
