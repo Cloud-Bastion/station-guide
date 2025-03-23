5e1d1dc3cbe412a40c978aa6d54926df4bba22f9
Melvin Schneider <melvinschneider02@gmail.com>
2025-03-23 01:03:08 +0100

    feat: Add expire product logic and state management

--- station-frontend/src/service/ExpireProductService.ts
+++ station-frontend/src/service/ExpireProductService.ts
@@ -1,17 +1,27 @@
 import settings from "./settings/Settings";
+import ExpireProductService from "@/service/ExpireProductService";
 
 const API_URL = "/v1/expire/products";
 
 export interface ExpireProductCategory {
     name: string;
     reduceProductTime?: number;
+    showProducts: boolean;
+}
+
+export enum ExpireProductState {
+    REDUCE = "Reduzieren",
+    REDUCED = "Reduziert",
+    SORT_OUT = "Aussortieren",
+    SET_DATE = "Datum setzen"
 }
 
 export interface ExpireProduct {
+    id: string;
     productId: number;
     name: string;
     reduceProductTime?: number | undefined;
-    expireDate?: Date | undefined;
+    expireDate?: Date | undefined | null;
     lastUpdateDate?: Date | undefined;
     category?: ExpireProductCategory | undefined | null;
 }
@@ -19,6 +29,49 @@ export interface ExpireProduct {
 export default {
     async getExpiringItems(): Promise<ExpireProduct[]> {
         const response = await settings.apiClient.get<ExpireProduct[]>(API_URL)
-        return response.data;
+        return response.data.map(product => {
+            if (product.lastUpdateDate !== undefined && product.lastUpdateDate !== null)
+                product.lastUpdateDate = new Date(product.lastUpdateDate + "T23:59:59")
+            return product;
+        });
+    },
+
+    async updateLastChange(product: ExpireProduct) {
+        const response = await settings.apiClient.patch(API_URL + "/" + product.id, {
+            id: product.id
+        })
+    },
+
+    async updateExpireDate(product: ExpireProduct) {
+        console.log("LOGGING FOR JAN")
+        const response = await settings.apiClient.patch(API_URL + "/" + product.id, {
+            id: product.id,
+            expireDate: product?.expireDate,
+            updateLastModifiedDate: false
+        })
+    },
+
+    compareDates(date: Date, secondDate: Date): boolean {
+        return (date.getDay() === secondDate.getDay()) &&
+            (date.getMonth() === secondDate.getMonth()) &&
+            (date.getFullYear() === secondDate.getFullYear());
+    },
+
+    getState(product: ExpireProduct): ExpireProductState {
+        let currentDate = new Date(Date.now());
+
+        if (product.expireDate === undefined || product.expireDate === null) {
+            return ExpireProductState.SET_DATE;
+        }
+
+        if (product.lastUpdateDate !== null && product.lastUpdateDate !== undefined && ExpireProductService.compareDates(product.lastUpdateDate, currentDate)) {
+            return ExpireProductState.REDUCED;
+        }
+
+        if (product.expireDate < currentDate) {
+            return ExpireProductState.SORT_OUT;
+        }
+
+        return ExpireProductState.REDUCE
     }
 }
