09b7829d530383959efd7e4e070c2ccb615de04f
Melvin Schneider <melvinschneider02@gmail.com>
2025-03-23 09:31:24 +0100

    refactor: Configure CORS and allow frontend origin.

--- station-resource-server/src/main/kotlin/dev/aventix/station/resource/server/security/SecurityConfiguration.kt
+++ station-resource-server/src/main/kotlin/dev/aventix/station/resource/server/security/SecurityConfiguration.kt
@@ -1,10 +1,13 @@
 package dev.aventix.station.resource.server.security
 
-import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.disable
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -15,25 +18,21 @@ class SecurityConfiguration {
     @Bean
     @Throws(Exception::class)
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.cors {
-            disable()
+        return http.cors {
+            it.configurationSource(UrlBasedCorsConfigurationSource().apply {
+                registerCorsConfiguration("/**", CorsConfiguration().applyPermitDefaultValues().apply {
+                    addAllowedMethod(HttpMethod.OPTIONS)
+                    addAllowedMethod(HttpMethod.POST)
+                    addAllowedMethod(HttpMethod.PATCH)
+                    addAllowedMethod(HttpMethod.DELETE)
+                    addAllowedOrigin("http://localhost:5173")
+                })
+            })
         }.csrf {
-            disable()
+            it.disable()
         }.authorizeHttpRequests { auth ->
             auth.anyRequest().permitAll()
-        }
-        //TODO: Add auth layer
-        return http.build()
+        }.build()
     }
 
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            }
-        }
-    }
 }
