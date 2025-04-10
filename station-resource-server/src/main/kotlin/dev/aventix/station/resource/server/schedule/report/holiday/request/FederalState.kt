8093a7c9a37ba7cada1675195b1657abbdff9378
Raphael <43720275+PXAV@users.noreply.github.com>
2025-04-10 12:00:56 +0200

    add: create enum for german federal states and their abbreviations

---
+++ station-resource-server/src/main/kotlin/dev/aventix/station/resource/server/schedule/report/holiday/request/FederalState.kt
@@ -0,0 +1,22 @@
+package dev.aventix.station.resource.server.schedule.report.holiday.request
+
+enum class FederalState(val abbreviation: String) {
+
+    BADEN_WURTTEMBERG("bw"),
+    BAVARIA("by"),
+    BERLIN("be"),
+    BRANDENBURG("bb"),
+    BREMEN("hb"),
+    HAMBURG("hh"),
+    HESSE("he"),
+    MECKLENBURG_VORPOMMERN("mv"),
+    LOWER_SAXONY("ni"),
+    NORTH_RHINE_WESTPHALIA("nw"),
+    SAARLAND("sl"),
+    RHINELAND_PALATINATE("rp"),
+    SAXONY("sn"),
+    SAXONY_ANHALT("st"),
+    SCHLESWIG_HOLSTEIN("sh"),
+    THURINGIA("th");
+
+}
